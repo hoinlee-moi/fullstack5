9dfee029e56dc1d26607b3edc55bb34e0c7c70ef
hoinlee-moi <lhi3048@gmail.com>
2023-11-09 20:47:53 +0900

    trythis: my Promise then chaining,catch,finally

--- trythis/promise/myPromise.js
+++ trythis/promise/myPromise.js
@@ -10,38 +10,38 @@ const randTime = (val) =>
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     const now = Date.now();
-    if (now % 2 >= 0) resolve(console.log("fulfill", now));
+    if (now % 2 < 0) resolve(console.log("fulfill", now));
     else reject(new Error("어디로?"));
   }, 1000);
 });
 
-// console.log("111>>", p);
-// setTimeout(() => console.log("222>>", p), 2000);
-// setTimeout(() => console.log("3>>", p), 4000);
+console.log("111>>", p);
+setTimeout(() => console.log("222>>", p), 2000);
+setTimeout(() => console.log("3>>", p), 4000);
 
-p.then((res) => {
-  console.log("p.then.res11>>>", res);
-  return randTime(1);
-})
-  .then((res) => randTime(2))
-  .then((res) => {
-    console.log("p.then.res22>>>", res);
-    return "FiNALLY";
-  })
-  .then(console.log("p.then.res33!!!"))
-  .then((res) => res || "TTT")
-  .catch((err) => console.error("err-11>>", err))
-  .catch((err) => console.error("err-22>>", err))
-  .finally(() => console.log("finally-11"))
-  .finally(() => console.log("finally-22"));
+// p.then((res) => {
+//   console.log("p.then.res11>>>", res);
+//   return randTime(1);
+// })
+//   .then((res) => randTime(2))
+//   .then((res) => {
+//     console.log("p.then.res22>>>", res);
+//     return "FiNALLY";
+//   })
+//   .then(console.log("p.then.res33!!!"))
+//   .then((res) => console.log("TTT"))
+//   .catch((err) => console.error("err-11>>", err))
+//   .catch((err) => console.error("err-22>>", err))
+//   .finally(() => console.log("finally-11"))
+//   .finally(() => console.log("finally-22"));
 
 function Promise(cb) {
   this.state = "Pending";
-  let thenFn = [];
+  const thenFn = [];
+  const finalFn = [];
   let catchFn;
-  let finallyFn = [];
-  Promise.prototype.then = (onFullfill) => {
-    thenFn.push(onFullfill);
+  Promise.prototype.then = (...onFullfill) => {
+    thenFn.push(...onFullfill);
     return this;
   };
   Promise.prototype.catch = (onReject) => {
@@ -51,17 +51,31 @@ function Promise(cb) {
   Promise.prototype.resolve = (res) => {
     this.state = "resolve";
     const fn = thenFn.shift();
-    if (fn) fn(res);
+    if (fn) {
+      const result = fn(res);
+      if (result instanceof Promise)
+        return result.then(...thenFn).finally(...finalFn);
+      else return this.resolve(result);
+    }
+    if (thenFn.length === 0) return this.final();
+
+    this.resolve(fn);
   };
   Promise.prototype.reject = (err) => {
     this.state = "reject";
-    catchFn(err);
+    if (catchFn) catchFn(err);
+    if (finalFn) this.final();
   };
-  Promise.prototype.finally = (callback) => {
-    if (cb !== undefined && thenFn.length === 0 && catchFn.length === 0)
-      callback();
-    this.state = "setteled";
+  Promise.prototype.finally = (...callback) => {
+    finalFn.push(...callback);
     return this;
+  };
+  Promise.prototype.final = () => {
+    const finallyFn = finalFn.shift();
+    if (finallyFn) {
+      finallyFn();
+      this.final();
+    }
   };
 
   cb(this.resolve, this.reject);
@@ -73,3 +87,18 @@ function Promise(cb) {
 resolve나 reject가 실행될 때 다시 Promise를 실행하게 하고 클로저를 사용해서 저장된 값을 확인하고
 이때 pending 상태면 
 */
+
+// Object.defineProperties(this, {
+//   thenFn: {
+//     enumerable: false,
+//     writable: true,
+//   },
+//   catchFn: {
+//     enumerable: false,
+//     writable: true,
+//   },
+//   finallyFn: {
+//     enumerable: false,
+//     writable: true,
+//   },
+// });
