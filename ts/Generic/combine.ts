34e8ca2b85788f4b982e3e7e4ff34b4bd26a9d9e
hoinlee-moi <lhi3048@gmail.com>
2023-11-22 19:54:33 +0900

    ts : trythis Combine Make, shopinglist

--- ts/Generic/combine.ts
+++ ts/Generic/combine.ts
@@ -8,29 +8,46 @@ type IDept = {
   id: number;
   age: string;
   dname: string;
-  captain: string;
+  captain: boolean;
 };
 
 type Differ<K, Type> = K extends Type ? never : K;
 
-type InterProperty<T, V> = {
-  [key in keyof (T | V)]: T[key] | V[key];
+type InterProperty<T, U> = {
+  [key in keyof (T | U)]: T[key] | U[key];
 };
 
-type Combine<T, V> = InterProperty<T, V> & {
-  [key in Differ<keyof (T & V), keyof (T | V)>]: (T & V)[key];
+type Combine<T, U> = InterProperty<T, U> & {
+  [key in Differ<keyof (T & U), keyof (T | U)>]: (T & U)[key];
+};
+
+type Combine2<T, U> = {
+  [key in keyof (T & U)]: (T & U)[key] extends never
+    ? key extends keyof T & keyof U
+      ? T[key] | U[key]
+      : never
+    : (T & U)[key];
+};
+type Combine3<T, U> = {
+  [key in keyof (T & U)]: (T & U)[key] extends never
+    ?
+        | T[key extends keyof T ? key : never]
+        | U[key extends keyof U ? key : never]
+    : (T & U)[key];
 };
 
 type ICombined = Combine<IUser, IDept>;
-// type A = InterProperty<IUser, IDept>;
+type ICombined2 = Combine2<IUser, IDept>;
+type ICombined3 = Combine3<IUser, IDept>;
 
+// type A = InterProperty<IUser, IDept>;
 
 const a: ICombined = {
   id: 2,
   age: 2,
   name: "dd",
   dname: "ss",
-  captain: "ss",
+  captain: false,
 };
 export {};
 
